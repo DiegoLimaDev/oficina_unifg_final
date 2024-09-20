7518cbbc1a094bbdc2036eb256d7abbafc8af174
Diego Lima <xxdiegolsxx@gmail.com>
2024-09-20 18:42:08 -0300

    theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+export const theme = {
+  colors: {
+    primaryColor: "#042940",
+    secondaryColor: "#005C53",
+    thirdColor: "#9FC131",
+    fourthColor: "#DBF227",
+    fifthColor: "#D6D58E",
+    white: "#F5F5F5",
+    black: "#00040D",
+  },
+  sizes: {
+    xsmall: "1rem",
+    small: "1.6rem",
+    medium: "2rem",
+    big: "3rem",
+  },
+};
