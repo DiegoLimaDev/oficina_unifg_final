7353fea252861882226b72bbadb5eb309b870ccb
Diego Lima <xxdiegolsxx@gmail.com>
2024-09-20 18:41:42 -0300

    GlobalStyles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { GlobalStyles } from "@mui/material";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <GlobalStyles
+      styles={{
+        body: { margin: 0, padding: 0, fontSize: "62.5%" },
+      }}
+    />
     <App />
   </StrictMode>,
-)
+);
